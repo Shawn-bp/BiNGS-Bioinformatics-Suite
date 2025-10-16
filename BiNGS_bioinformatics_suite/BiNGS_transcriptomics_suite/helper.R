4ebf17589a61bdf4f0134f551611634e9dcb423c
Shawn-bp <shawnfridlyand@gmail.com>
2025-10-16 16:11:16 -0400

    Update helper.R

--- BiNGS_bioinformatics_suite/BiNGS_transcriptomics_suite/helper.R
+++ BiNGS_bioinformatics_suite/BiNGS_transcriptomics_suite/helper.R
@@ -1,11 +1,13 @@
 #------------------- PCA specific variables ------------------- 
 pca_color_palette_list = c(
-  "Set1 (Bright)" = "Set1",
-  "Dark2 (Muted)" = "Dark2",
-  "Set2 (Soft Pastel)" = "Set2",
-  "Set3 (Vibrant Pastel)" = "Set3",
-  "Paired (Contrasting)" = "Paired",
-  "Accent (Bold)" = "Accent"
+  "Set1 (Bright reds/blues)" = "Set1",
+  "Dark2 (Darker tones)" = "Dark2",
+  "Paired (Light & dark pairs)" = "Paired",
+  "Accent (High contrast)" = "Accent",
+  "Set2 (Pastel)" = "Set2",
+  "Set3 (Very light)" = "Set3",
+  "Pastel1 (Soft)" = "Pastel1",
+  "Pastel2 (Muted)" = "Pastel2"
 )
 
 pca_metadata_columns_to_remove = c("species", "strandedness", "alignment_bam", "fastq_1", "fastq_2", "fastq_1_trimmed", "fastq_2_trimmed", "bings_sample_id", "bings_case_id", "bings_patient_id","file_name", "bings_block_id")
