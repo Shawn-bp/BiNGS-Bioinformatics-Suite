c9cf42943b5adf5858a3fb643042f630f94d2544
Shawn-bp <shawnfridlyand@gmail.com>
2026-02-20 08:58:35 -0500

    Update app.R

--- BiNGS_bioinformatics_suite/BiNGS_transcriptomics_suite/app.R
+++ BiNGS_bioinformatics_suite/BiNGS_transcriptomics_suite/app.R
@@ -700,15 +700,13 @@ server <- function(input, output, session) {
   })
   
   # ---- Reactive: compute distance matrix (updates immediately) ----
-  dist_matrix_reactive <- eventReactive(input$sample_distance_run_button, {
+  dist_matrix_reactive <- reactive({
     req(count_data(), sample_metadata())
-    
-    samples_remove <- input$sample_distance_samples_to_remove
     
     run_sample_distance(
       counts = count_data(),
       metadata = sample_metadata(),
-      remove_samples = samples_remove,
+      remove_samples = input$sample_distance_samples_to_remove,
       scale_type = input$sample_distance_heatmap_scaling_type,
       ntop = 500
     )
